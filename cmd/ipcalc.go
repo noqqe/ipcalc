98b181459b6d555b5dde418488a788430dbeeece
Florian Baumann <flo@noqqe.de>
2026-04-02 13:03:34 +0200

    apply go std repo structure

---
+++ cmd/ipcalc.go
@@ -0,0 +1,7 @@
+package main
+
+import cmd "github.com/noqqe/ipcalc/pkg/ipcalc"
+
+func main() {
+	cmd.Execute()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/noqqe/ipcalc/cmd"
-
-func main() {
-	cmd.Execute()
-}
